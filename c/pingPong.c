752cbb056151821e8f1b173f82d3ed771a723252
ojsj18 <juhnior88@gmail.com>
2023-02-08 16:16:04 -0300

    funciona com roots diferentes

--- c/pingPong.c
+++ c/pingPong.c
@@ -5,8 +5,17 @@
 #include <math.h>
 #include "chrono.c"
       
+#include <assert.h>
 
-void myBCAST(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm){
+#define USE_MPI_Bcast 1  // do NOT change
+#define USE_my_Bcast 2   // do NOT change
+//choose either BCAST_TYPE in the defines bellow
+//#define BCAST_TYPE USE_MPI_Bcast
+#define BCAST_TYPE USE_my_Bcast
+
+const int SEED = 100;
+
+void my_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm){
 
     int size, rank, pos;
 
@@ -16,14 +25,15 @@ void myBCAST(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm 
     int source, target;
 
     int limite = ceil(log2(size));
+    int rank_r = (rank - root + size) % size;
 
     for (int i = 0; i < limite; i++)
     {
         MPI_Status status;
         pos = pow(2, i);
 
-        target = (rank+pos);
-        source = (rank-pos);
+        target = (rank+pos) % size;
+        source = (root + rank_r-pos) %size;
 
         if (target < size && rank < pos)
         {
@@ -34,6 +44,49 @@ void myBCAST(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm 
           MPI_Recv(buffer, count, datatype, source, 0, comm, &status);
         }
     } 
+}
+
+void verifica_my_Bcast( void *buffer, int count, MPI_Datatype datatype,
+                        int root, MPI_Comm comm )
+{
+    int comm_size;
+    int my_rank;
+    
+    MPI_Comm_size( comm, &comm_size );
+    MPI_Comm_rank( comm, &my_rank );
+    static long *buff = (long *) calloc( count*comm_size, sizeof(long) );
+    
+    
+    // preenche a faixa do raiz com alguma coisa (apenas no raiz)
+    if( my_rank == root )
+       for( int i=0; i<count; i++ )
+          buff[ i ] = i+SEED;
+    
+    #if BCAST_TYPE == USE_MPI_Bcast
+       MPI_Bcast( buff, count, datatype, root, comm );
+    #elif BCAST_TYPE == USE_my_Bcast
+       my_Bcast( buff, count, datatype, root, comm );
+    #else
+       assert( BCAST_TYPE == USE_MPI_Bcast || BCAST_TYPE == USE_my_Bcast );
+    #endif   
+	   
+    
+    // cada nodo verifica se sua faixa recebeu adequadamente o conteudo
+    int ok=1;
+    int i;
+    for( i=0; i<count; i++ )
+       if( buff[ i ] != i+SEED ) {
+          ok = 0;
+          break;
+       }
+    // imprime na tela se OK ou nao
+    if( ok )
+        fprintf( stderr, "MY BCAST VERIF: node %d received ok\n", my_rank );
+    else
+        fprintf( stderr, "MY BCAST VERIF: node %d NOT ok! local position: %d contains %ld\n",
+                           my_rank, i, buff[i] );
+
+   free(buff);      
 }
 
 int main(int argc, char* argv[]) {
@@ -117,7 +170,7 @@ int main(int argc, char* argv[]) {
 
 
     for(int i=0;i < nMsg;i++){
-      myBCAST(ping, ni, MPI_LONG, 0, MPI_COMM_WORLD);
+      my_Bcast(ping, ni, MPI_LONG, 0, MPI_COMM_WORLD);
     }
 
 
@@ -142,6 +195,6 @@ int main(int argc, char* argv[]) {
 	}
 
   printf("Rank: %d, init : %ld, final: %ld \n", rank, ping[0], ping[ni-1]);
-
+  verifica_my_Bcast( ping, ni, MPI_LONG, 0, MPI_COMM_WORLD );
   MPI_Finalize();
 }
